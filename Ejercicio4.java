08a5458fafd834c16d004852f2bc5869d8cd6b00
Erick Chavez Coronado <ecjeux24@gmail.com>
2025-03-06 11:04:24 -0600

    Corrección del Ejercicio4.java

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,39 +1,44 @@
 package com.generation;
+import java.util.Scanner; // se nombra la libreria para el uso del scanner
 
 public class Codigo4 {
 
-    Scanner s = new Scanner();
-    
-    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-    String j1 = s.nextLine();
-    
-    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-    Scanner s2 = new Scanner();
-    String j2 = s.nextLine();
-    
-    if (j1 == j2)) {
-      System.out.println("Empate");
-    } else {
-      int g = 2;
-      switch(j1) {
-        case "piedra":
-          if (j2 == "tijeras") {
-            g = 1;
-          }
+public statatic void main (String[] args){ // hacia falta la funcion para correr el codigo
 
-        case "papel":
-          if (j2 == "piedra") {
-            g = 1;
-          
-        case "tijera":
-          if (j2.equals("papel")) {
-            g = 1;
-          }
-          break;
-        default:
+Scanner s = new Scanner( system.in); // instanciamos para recibir el dato
+System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
+String j1 = s.next();
+
+System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
+Scanner s2 = new Scanner( system.in);
+String j2 = s2.next(); // llamamos de manera correcta la variable
+
+if (j1 == j2)) {
+  System.out.println("Empate");
+} else {
+  int g = 2;
+  switch(j1) {
+    case "piedra":
+      if (j2 == "tijeras") {
+        g = 1;
       }
-      System.out.println("Gana el jugador " + g);
-    }
+    case "papel":
+      if (j2 == "piedra") {
+        g = 1;
+  }
+    case "tijera":
+      if (j2=="papel") {
+        g = 1;
+      }
+      break;
+    default:
+         g=2; // si ninguno de los casos se cumple gana 2
+  }
+  System.out.println("Gana el jugador " + g);
+}
+}
+
+}
   
   
 }
